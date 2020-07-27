faeafc49b4ef10c134762a57d4952a3f5be01498
SHoeks <36075931+SHoeks@users.noreply.github.com>
2020-07-27 11:59:03 +0200

    Update load_spatial_inputs_0.5.R

--- load_spatial_inputs_0.5.R
+++ load_spatial_inputs_0.5.R
@@ -6,13 +6,13 @@ load_spatial_inputs_0.5 = function(wd)
   
   if(!file.exists(paste0(wd,"/0.5degree.zip"))){
       cat('Downloading zip from github repository: ')
-      download.file(url = "https://github.com/SHoeks/MadingleyR_0.5degree_inputs/archive/master.zip", destfile = "0.5degree.zip"); 
+      download.file(url = "https://github.com/SHoeks/MadingleyR_0.5degree_inputs/archive/master.zip", destfile = paste0(wd,"/0.5degree.zip")); 
   }else{
     cat('Zip already downloaded \n')
   }
   if(!file.exists(paste0(wd,"/MadingleyR_0.5degree_inputs-master"))){
       cat('Extracting zip \n')
-      unzip(zipfile = "0.5degree.zip")
+      unzip(zipfile = paste0(wd,"/0.5degree.zip"))
   }else{
     cat('Zip already extracted \n')
   }
